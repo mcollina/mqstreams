fe10e1fff571f454a6f0a351caff76083641b792
Matteo Collina <matteo.collina@gmail.com>
2014-03-01 12:30:49 +0100

    Added support for closing readable streams.

--- index.js
+++ index.js
@@ -25,13 +25,41 @@ function MQReadable(mq, topic, opts) {
 
   streams.Transform.call(this, opts)
 
+  this.topic = topic;
+  this.mq = mq
+
   var that = this;
-  mq.on(topic, function(message, cb) {
+  this._callback = function forward(message, cb) {
     that.write(message, cb)
-  })
+  }
+
+  mq.on(topic, this._callback)
 }
 
 util.inherits(MQReadable, streams.PassThrough)
+
+MQReadable.prototype.close = function() {
+  this.end()
+}
+
+MQReadable.prototype._flush = function(callback) {
+  this.mq.removeListener(this.topic, this._callback)
+  callback()
+  this.emit('close')
+}
+
+MQReadable.prototype.pipe = function(destination, options) {
+  var that = this
+
+  function closeAnyway() {
+    that.close()
+  }
+
+  destination.on('finish', closeAnyway)
+  destination.on('error', closeAnyway)
+
+  streams.Transform.prototype.pipe.call(this, destination, options)
+}
 
 function MQWritable(mq, topic, opts) {
 
@@ -47,7 +75,6 @@ function MQWritable(mq, topic, opts) {
 util.inherits(MQWritable, streams.Writable)
 
 MQWritable.prototype._write = function(data, encoding, cb) {
-  console.log('aaa')
   this.mq.emit(data, cb)
 }
 
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var test = require('tap').test
   , mq = require('mqemitter')
   , mqstreams = require('./')
+  , streams = require('readable-stream')
 
 test('supports readable', function(t) {
   t.plan(1)
@@ -38,6 +39,85 @@ test('avoid overflow in readable', function(t) {
 
   stream.on('data', function(message) {
     called = true
+  })
+})
+
+test('close a readable stream', function(t) {
+  t.plan(3)
+
+  var e = mqstreams(mq())
+    , expected = {
+          topic: 'hello world'
+        , payload: { my: 'message' }
+      }
+    , stream = e.readable('hello world')
+
+  e.emit(expected)
+
+  stream.on('end', function() {
+    t.ok(true, 'should emit end')
+  })
+
+  stream.on('close', function() {
+    t.ok(true, 'should emit close')
+  })
+
+  stream.once('data', function(message) {
+    stream.close()
+
+    stream.on('data', function() {
+      t.ok(false, 'should not emit again')
+    })
+
+    e.emit(expected, function() {
+      t.ok(true, 'should call the callback')
+    })
+  })
+})
+
+test('close a readable stream when downstream is closed', function(t) {
+  t.plan(1)
+
+  var e = mqstreams(mq())
+    , expected = {
+          topic: 'hello world'
+        , payload: { my: 'message' }
+      }
+    , stream = e.readable('hello world')
+    , writable = new streams.Writable({ objectMode: true })
+
+  writable._write = function(a, b, cb) { cb() }
+
+  stream.pipe(writable)
+  e.emit(expected, function() {
+    writable.end()
+  })
+
+  stream.on('close', function() {
+    t.ok(true, 'should emit close')
+  })
+})
+
+test('close a readable stream when downstream errors', function(t) {
+  t.plan(1)
+
+  var e = mqstreams(mq())
+    , expected = {
+          topic: 'hello world'
+        , payload: { my: 'message' }
+      }
+    , stream = e.readable('hello world')
+    , writable = new streams.Writable({ objectMode: true })
+
+  writable._write = function(a, b, cb) { cb() }
+
+  stream.pipe(writable)
+  e.emit(expected, function() {
+    writable.emit('error', new Error('fake error'))
+  })
+
+  stream.on('close', function() {
+    t.ok(true, 'should emit close')
   })
 })
 
